31823d8a0af69f0e2b460a8518650924a5630bdf
Fabio Rodrigues Vieira Costa <38567767+fabiotreze@users.noreply.github.com>
2024-08-19 17:04:26 -0300

    Update AzureArcSQLTags-Inheritance.ps1
    Add docs

--- AzureArcSQLTags-Inheritance.ps1
+++ AzureArcSQLTags-Inheritance.ps1
@@ -1,5 +1,30 @@
-# Requirements 
-# Install modules most recent: Az.Accounts and Az.ResourceGraph latest version
+<#
+.SYNOPSIS
+This script checks and updates tags for Azure Arc SQL Server instances based on the tags of associated hybrid machines.
+
+.DESCRIPTION
+The script connects to Azure using a user-assigned managed identity and retrieves Azure Arc SQL Server instances and their associated hybrid machines. It compares the tags of the hybrid machines with the tags of the SQL Server instances and determines the compliance status. If any non-compliant instances are found, it updates the tags of the SQL Server instances to match the tags of the hybrid machines.
+
+.PARAMETER ResourceGroupName
+The name of the resource group containing the Azure Arc SQL Server instances.
+
+.PARAMETER SubscriptionID
+The ID of the subscription containing the Azure Arc SQL Server instances.
+
+.PARAMETER tagName
+The name of the tag to compare and update.
+
+.EXAMPLE
+.\AzureArcSQLTags-Inheritance.ps1 -ResourceGroupName "myResourceGroup" -SubscriptionID "12345678-1234-1234-1234-1234567890ab" -tagName "centro_de_custo"
+
+.NOTES
+- This script requires the Az.Accounts and Az.ResourceGraph modules to be installed.
+- The script requires a user-assigned managed identity with appropriate permissions to connect to Azure and update tags.
+- The script uses the Azure Resource Graph to query and retrieve the Azure Arc SQL Server instances and their associated hybrid machines.
+- The script updates the tags of the SQL Server instances using the Update-AzTag cmdlet.
+- The compliance status is determined based on the comparison of tags between the hybrid machines and SQL Server instances.
+- Non-compliant instances are identified and their tags are updated to match the tags of the associated hybrid machines.
+#>
 
 # Define the parameters to be required at runtime
 param (
